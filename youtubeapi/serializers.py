25ee8843bad468af80195dad91ce2b3ac671093b
cseshahriar <cse.shahriar.hosen@gmail.com>
2020-12-15 13:34:41 +0600

    serializers

---
+++ youtubeapi/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import Video, Rating
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = '__all__'
+        extra_kwargs = {'url': {'required': True}}
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ['id', 'stars', 'user', 'video', 'comments']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {
+            'password': {'required': True, 'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
